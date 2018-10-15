f1cfee5fa438bcd2b9c0546da98a1f8a34351f64
oerling@fb.com <oerling@fb.com>
2018-10-15 15:54:26 -0700

    add

--- presto-main/src/main/java/com/facebook/presto/execution/TaskManagerConfig.java
+++ presto-main/src/main/java/com/facebook/presto/execution/TaskManagerConfig.java
@@ -48,7 +48,7 @@ public class TaskManagerConfig
     private DataSize maxLocalExchangeBufferSize = new DataSize(32, Unit.MEGABYTE);
     private DataSize maxIndexMemoryUsage = new DataSize(64, Unit.MEGABYTE);
     private boolean shareIndexLoading;
-    private int maxWorkerThreads = Runtime.getRuntime().availableProcessors() * 2;
+    private int maxWorkerThreads = 1 /*Runtime.getRuntime().availableProcessors() * 2; */
     private Integer minDrivers;
     private Integer initialSplitsPerNode;
     private int minDriversPerTask = 3;
